d80bd66f438f0d35335aab43ec4a1b22fdd58cb3
YamioKDL <altairteam@bk.ru>
2021-10-02 21:50:00 +0300

    (new): buttons style and loader

--- js/main.js
+++ js/main.js
@@ -5,10 +5,19 @@ window.onload = function () {
 let get_template = function (page) {
 	let request = new XMLHttpRequest();
 	let main_root = document.getElementById("root");
-	main_root.innerHTML = "";
+	main_root.innerHTML = `<span class="rotating_loader" />`;
 	request.open("GET", `pages/${page}.html`);
 	request.onload = function () {
 		main_root.innerHTML = request.response;
 	};
 	request.send();
 };
+
+let activate = function (target) {
+	document.querySelectorAll("button[selected]").forEach(function(element,index) {
+		element.removeAttribute("selected");
+	});
+	let attribute = document.getElementsByName(target.getAttribute("name"))[1]
+	attribute.setAttribute("selected",'')
+	target.setAttribute("selected",'');
+}
